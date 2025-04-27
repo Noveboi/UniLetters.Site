1b2e2fd67bb8ec537665d5bda8efe4157ab78b51
George Nikolaidis <cptgiorgos03@gmail.com>
2025-04-27 12:31:17 +0300

    Remove presets and start with clean slate

---
+++ src/renderer/src/components/Test.svelte
@@ -0,0 +1,13 @@
+<script>
+  const { message } = $props()
+</script>
+
+<h1 class="test-component">{message}</h1>
+
+<style>
+  .test-component {
+    background-color: azure;
+    color: darkblue;
+    padding: 1em;
+  }
+</style>
