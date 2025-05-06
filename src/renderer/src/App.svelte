ad70f8c342eebd0b6647e437230e1dccaf898997
MariaDim03 <marydimi03@gmail.com>
2025-05-06 16:56:19 +0300

    -Background & Title

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,6 +1,29 @@
 <script>
-  import Test from './components/Test.svelte'
+  import Title from './components/Title.svelte'
 </script>
 
-<h1>Hello!</h1>
-<Test message="Hi There!" />
+<div class="background">
+  <Title message="Uni Letters" />
+</div>
+
+<style>
+  :global(html, body) {
+    display: block;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+
+  .background {
+    position: fixed; 
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('/images/notebook-background.svg');
+    background-size: cover; 
+    background-repeat: no-repeat;
+    background-position: center;
+    z-index: -1;
+  }
+</style>
---
+++ src/renderer/src/components/Title.svelte
@@ -0,0 +1,18 @@
+<script>
+  const { message } = $props()
+</script>
+
+<h1 class="title title-container">{message}</h1>
+
+<style>
+  .title {
+    color: white;
+    font-family: 'Gloria Hallelujah', cursive;
+    font-size: 2.2rem;
+  }
+
+  .title-container {
+    display: flex;
+    justify-content: center;
+  }
+</style>
