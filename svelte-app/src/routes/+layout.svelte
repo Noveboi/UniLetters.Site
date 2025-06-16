a1b465e495c5040b01abff73962c8e1613acf116
George Nikolaidis <nove@novesoftware.net>
2025-06-16 19:04:16 +0300

    Adjust layout

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -4,24 +4,49 @@
 
     const { children } = $props();
 </script>
+<div class="bg">
+    <div class="background"></div>
+</div>
 
-<div class="background"></div>
-<Title message="Uni Letters" />
+<div class="app">
+    <div class="header">
+        <Title message="Uni Letters" />
+    </div>
 
-{@render children()}
+    <main>
+        {@render children()}
+    </main>
+</div>
 
 <style>
-    .background {
+    .app {
+        width: 100vw;
+        height: 100vh;
+    }
+
+    .header {
+        position: sticky;
+        top: 0;
+        background: black;
+    }
+
+    .bg {
         position: fixed; 
         top: 0;
         left: 0;
         width: 100vw;
         height: 100vh;
+        z-index: -1;
+        background: black;
+    }
+
+    .background {
+        width: 100%;
+        height: 100%;
         background-image: url('/images/notebook-background.svg');
         background-size: cover; 
         background-repeat: no-repeat;
         background-position: center;
-        background-attachment: fixed;
         z-index: -1;
     }
 </style>
